462e4f6187fd4198c9b6116cf9e917307ec17ebb
alejandroleon98 <alexleon8898@gmail.com>
2020-04-25 18:35:28 +0200

    w1 and w2

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,46 +1,65 @@
-module.exports = function(app, swig, gestorBD) {
+module.exports = function (app, swig, gestorBD) {
 
-    app.get("/usuarios", function(req, res) {
+    app.get("/usuarios", function (req, res) {
         res.send("ver usuarios");
     });
 
-    app.get("/registrarse", function(req, res) {
+    app.get("/registrarse", function (req, res) {
         let respuesta = swig.renderFile('views/bregistro.html', {});
         res.send(respuesta);
     });
 
-    app.post('/usuario', function(req, res) {
+    app.post('/usuario', function (req, res) {
+        //password
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
+        //repeat password
+        let seguro2 = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.repeatPassword).digest('hex');
         let usuario = {
             email: req.body.email,
-            password: seguro
+            name: req.body.name,
+            lastname: req.body.lastname,
+            password: seguro,
+            repeatPassword: seguro2
         }
-        gestorBD.insertarUsuario(usuario, function (id) {
-            if (id == null) {
-                res.redirect("/registrarse?mensaje=Reeor al registrar usuario");
-            } else {
-                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
-            }
-        });
+        //Si las contraseñas coinciden
+        if (usuario.password != usuario.repeatPassword)
+            res.redirect("/registrarse?mensaje=Las contraseñas no coinciden");
+        else {
+            //Si el email no existe en base de datos ya
+            criterio = {"email": usuario.email};
+            gestorBD.obtenerUsuarios(criterio, function (usuariobd) {
+                if (usuariobd.length == 0)
+                    gestorBD.insertarUsuario(usuario, function (id) {
+                        if (id == null) {
+                            res.redirect("/registrarse?mensaje=Error al registrar usuario");
+                        } else {
+                            res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                        }
+                    });
+                else
+                    res.redirect("/registrarse?mensaje=Este email ya está registrado");
+            });
+        }
     });
 
-    app.get("/identificarse", function(req, res) {
+    app.get("/identificarse", function (req, res) {
         let respuesta = swig.renderFile('views/bidentificacion.html', {});
         res.send(respuesta);
     });
 
-    app.post("/identificarse", function(req, res) {
+    app.post("/identificarse", function (req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         let criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.redirect("/identificarse"+"?mensaje=Email o password incorrecto"+"&tipoMensaje=alert-danger ");
+                res.redirect("/identificarse" + "?mensaje=Email o password incorrecto" + "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.redirect("/publicaciones");
@@ -53,15 +72,15 @@ module.exports = function(app, swig, gestorBD) {
         res.send("Usuario desconectado");
     });
 
-    app.get("/publicaciones", function(req, res) {
-        let criterio = { autor : req.session.usuario };
-        gestorBD.obtenerCanciones(criterio, function(canciones) {
+    app.get("/publicaciones", function (req, res) {
+        let criterio = {autor: req.session.usuario};
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
             if (canciones == null) {
                 res.send("Error al listar ");
             } else {
                 let respuesta = swig.renderFile('views/bpublicaciones.html',
                     {
-                        canciones : canciones
+                        canciones: canciones
                     });
                 res.send(respuesta);
             }
