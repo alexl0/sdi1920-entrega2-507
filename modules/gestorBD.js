ca6fe1c71868bde08c1ebd3e536642ef19132d24
alejandroleon98 <alexleon8898@gmail.com>
2020-05-11 12:41:42 +0200

    solucionar bug de la paginacion en amigos. va perfecta la paginacion ahora

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -297,7 +297,7 @@ module.exports = {
                     ]
                 }
                 let collection = db.collection('amigos');
-                collection.count(function (err, count) {
+                collection.count(criterio, function (err, count) {
                     collection.find(criterio).skip((pg - 1) * 5).limit(5)
                         .toArray(function (err, amigos) {
                             if (err) {
